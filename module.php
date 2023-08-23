00e8a866c90cc61a930bf0a670de032a586e181e
gaouimounir <mounir.gaoui@outlook.com>
2023-08-23 20:48:06 +0200

    feat:created getter and setter module class

--- module.php
+++ module.php
@@ -14,4 +14,32 @@ class Module{
         $this->date_ajout = $date_ajout;
         $this->etat = $etat;
     }
+    function getId_module(){
+        return $this->id_module;
+    }
+    function getNom(){
+        return $this->nom;
+    }
+    function setNom($nom){
+        $this->nom = $nom;
+    }
+    function getType(){
+        return $this->type;
+    }
+    function setType($type){
+        $this->type = $type;
+    }
+    function getDate_ajout(){
+        return $this->date_ajout;
+    }
+    function setDate_ajout($date_ajout){
+        $this->date_ajout = $date_ajout;
+    }
+    function getEtat(){
+        return $this->etat;
+    }
+    function setEtat($etat){
+        $this->etat = $etat;
+    }
+    
 }
