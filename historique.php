2cd4a77365ef2e2698f3b87da6b7204cdd86c971
gaouimounir <mounir.gaoui@outlook.com>
2023-08-24 14:18:48 +0200

    feat:created constructeur historique

--- historique.php
+++ historique.php
@@ -7,7 +7,13 @@ class Historique{
     private $etat;
     private $id_module;
 
-    
+    function __construct($id_historique, $time_value, $valeur_mesurer, $etat, $id_module){
+        $this->id_historique = $id_historique;
+        $this->time_value = $time_value;
+        $this->valeur_mesurer = $valeur_mesurer;
+        $this->etat = $etat;
+        $this->id_module = $id_module;
+    }
 
 
     }
