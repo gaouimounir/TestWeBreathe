a6b039f5996c8a5cd62b890f82c96e689c049897
gaouimounir <mounir.gaoui@outlook.com>
2023-08-25 20:38:44 +0200

    feat:created notification constructor

--- notification.php
+++ notification.php
@@ -5,4 +5,12 @@ class Notification{
     private $contenu_notif;
     private $time_notif;
     private $id_module;
+
+    function __construct($id_notif, $contenu_notif, $time_notif, $id_module){
+        $this->id_notif = $id_notif;
+        $this->contenu_notif = $contenu_notif;
+        $this->time_notif = $time_notif;
+        $this->id_module = $id_module;
+    }
+
     }
