1b19dbfb40f38fd714fc5c7f71ecf860a630e7b6
gaouimounir <mounir.gaoui@outlook.com>
2023-08-25 20:58:42 +0200

    feat:created notification methode

--- notification.php
+++ notification.php
@@ -32,4 +32,17 @@ class Notification{
         return $this->id_module;
     }
 
+    static function getNotificationById($id_notif){
+        $bdd = new PDO('mysql:dbname=testwebreathe;host=localhost','root','');
+        $query = $bdd->query('SELECT * FROM notification WHERE id_notif = ' . $id_notif);
+        $uneNotification = $query->fetch();
+        return new Notification($uneNotification['id_notif'], $uneNotification['contenu_notif'], $uneNotification['time_notif'], $uneNotification['id_module']);
+    }
+
+    static function updateNotification($notification){
+        $bdd = new PDO('mysql:dbname=testwebreathe;host=localhost','root','');
+        $query = $bdd->prepare('UPDATE notification SET contenu_notif = :contenu_notif, time_notif = :time_notif, id_module = :id_module WHERE id_notif = :id_notif');
+        $query->execute(array('contenu_notif' =>$notification->getContenu_notif(), 'time_notif' =>$notification->getTime_notif(), 'id_module' =>$notification->getId_module));
+    }
+
     }
