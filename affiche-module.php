f6170138b5a649122206d0a0a54ded71a8d3967c
gaouimounir <mounir.gaoui@outlook.com>
2023-08-28 21:16:39 +0200

    feat:requete bdd recup module

---
+++ affiche-module.php
@@ -0,0 +1,32 @@
+<?php
+
+$bdd = new PDO('mysql:dbname=testwebreathe;host=localhost','root','');
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Affiche Module</title>
+</head>
+<body>
+    <?php
+    $recupModule = $bdd->query('SELECT * FROM module');
+    while ($module = $recupModule->fetch()){
+    ?>
+        <div>
+            <ul>
+                <li>Nom: <?= $module['nom'];?></li>
+                <li>Type: <?= $module['type'];?></li>
+                <li>Mesure : <?= $module['mesure'];?></li>
+                <li>Unite : <?= $module['unite'];?></li>
+                <li>Etat : <?= $module['etat'];?></li>
+            </ul>
+        </div>
+    <?php
+    }
+    ?>
+</body>
+</html>
